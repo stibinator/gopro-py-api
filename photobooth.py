b5c7b519cf6bbb137d99db15bd1cfef8bd7d479d
stib <stib@pureandapplied.com.au>
2019-08-01 13:31:13 +1000

    fixed the mess I made last night

--- photobooth.py
+++ photobooth.py
@@ -1,30 +1,35 @@
 from goprocam import GoProCamera, constants
-import time
-import qrcode
-# import ffmpeg
-import os
 from datetime import datetime
+import time, qrcode, keyboard, os, subprocess, platform
+# import ffmpeg #for adding overlays etc.
 
 class photoBooth(object):
-	def __init__(self, outputPath = "goProVids", videoLength = 5, settings = ("1080p","120"), baseURL = "localhost"):
+	def __init__(self, outputPath = "goProVids", videoLength = 3, settings = ("1080p","120"), baseURL = "localhost"):
 		try:
 			os.mkdir(outputPath)
 			print('created output folder' , outputPath)
 		except FileExistsError:
 			print("Directory ", outputPath, " will be used for video output")
 		self.videoLength = videoLength
+		self.settings = settings
 		self.outputPath = outputPath
+		self.baseURL = baseURL # base URL is the external facing URL the user connects to with the QR code
 		self.lastVideo = None
 		try:
-			self.gpCam = GoProCamera.GoPro()
-			self.gpCam.video_settings = settings
-			self.baseURL = baseURL
-			self.gpCam.gpControlSet(constants.Video.PROTUNE_VIDEO, constants.Video.ProTune.ON)
-			# sync the time of the camera to the computer
-			self.gpCam.syncTime()
+			self.gpCam = self.connectCamera()
 		except:
-			print("there was a problem connecting to the camera.") #well duh
-			#do some error handling stuff here..?
+			print("there was an error connecting to the camera")
+			self.gpCam = None
+
+	def connectCamera(self):
+		theCamera = GoProCamera.GoPro()
+		theCamera.video_settings = self.settings
+		# protune turns on more controls on the camera
+		theCamera.gpControlSet(constants.Video.PROTUNE_VIDEO, constants.Video.ProTune.ON)
+		# sync the time of the camera to the computer
+		theCamera.syncTime()
+		return theCamera
+
 
 	def takeVideo(self):
 		now = datetime.now()
@@ -38,17 +43,43 @@ class photoBooth(object):
 
 	def downloadVideo(self):
 		print("downloading shot")
-		self.gpCam.downloadLastMedia(custom_filename=self.lastVideo)
+		self.gpCam.downloadLastMedia(path=self.outputPath, custom_filename=self.lastVideo)
+		
+	def playVideo(self):
+		# depending on the platform we can use openCV or a thrird party player like omxplayer on Raspberry Pi
+		# TODO append QR to the end of the video with ffmpeg
+		videoFilePath = os.path.join(self.outputPath, self.lastVideo)
+		if platform.system() == 'Darwin':       # macOS
+			subprocess.call(('open', videoFilePath))
+		elif platform.system() == 'Windows':    # Windows
+			os.startfile(videoFilePath)
+		else:                                   # linux variants
+			subprocess.call(('xdg-open', videoFilePath))
 
 	def showQR(self):
 		qrText = "http://" + '/'.join((self.baseURL, self.outputPath, self.lastVideo))
 		qr = qrcode.make(qrText)
 		qr.show()
 
-	def newSession(self):
+	def countDown(self):
+		# do fancy countdown shenannigans here, or…
 		for i in range(3, 0, -1):
 			print(i)
 			time.sleep(1)
-		self.takeVideo()
-		self.downloadVideo()
-		self.showQR()
+
+	def newPhotoBoothSession(self):
+		keepTrying = True
+		while (not self.gpCam and keepTrying):
+			print ("No camera is connected.")
+			keepTrying = input("Do you want to try to connect the camera? ").lower() == "y"
+			if (keepTrying):
+				self.connectCamera()
+
+		if (self.gpCam):
+			print ("press spacebar to start") 
+			keyboard.wait(" ") # wait for spacebar - use GPIO etc
+			self.countDown()
+			self.takeVideo()
+			self.downloadVideo()
+			self.playVideo()
+			self.showQR()
